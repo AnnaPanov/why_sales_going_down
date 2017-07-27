25efc1177d3df871509d54303e996ebf520e0411
Anna Panov <hannasl@hotmail.com>
2017-07-27 00:31:15 -0400

    new config files

---
+++ lib/pivot_page.py
@@ -0,0 +1,52 @@
+def generate_pivot_page(title, data_rows, rows, cols):
+    rows = ', '.join('"' + row + '"' for row in rows)
+    cols = ', '.join('"' + col + '"' for col in cols)
+    result = []
+    result.append('''
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>%s</title>
+
+        <!-- external libs from cdnjs -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+
+        <!-- PivotTable.js libs from ../dist -->
+        <link rel="stylesheet" type="text/css" href="https://pivottable.js.org/dist/pivot.css">
+        <script type="text/javascript" src="https://pivottable.js.org/dist/pivot.js"></script>
+        <style>
+            body {font-family: Verdana;}
+        </style>
+
+        <!-- optional: mobile support with jqueryui-touch-punch -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.3/jquery.ui.touch-punch.min.js"></script>
+
+    </head>
+    <body>
+        <script type="text/javascript">
+
+    $(function(){
+        $("#output").pivotUI(
+            [
+''' % title)
+    for row in data_rows:
+        result.append('  {')
+        for field in row:
+            result.append('"%s": "%s", ' % (field, row[field].replace('"','')))
+        result.append('},\n')
+    result.append('''
+            ],
+            {
+                rows: [%s],
+                cols: [%s],
+            }
+        );
+     });
+        </script>
+
+        <div id="output" style="margin: 30px;"></div>
+
+    </body>
+</html>''' % (rows, cols))
+    return ''.join(result)
