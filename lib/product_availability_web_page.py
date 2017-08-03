928e29ddf3a1b207e930766f82e9c28880cbf5e6
Anna Panov <hannasl@hotmail.com>
2017-08-03 01:29:45 -0400

    title

--- lib/product_availability_web_page.py
+++ lib/product_availability_web_page.py
@@ -348,7 +348,7 @@ _html_head = '''
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Low-Hanging Fruit</title>
+  <title>Low-Hanging Fruit: the things which we can easily improve</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
