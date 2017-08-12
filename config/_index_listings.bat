81799d2dbfd86458e8a522ec331481432fe96d04
Anna Panov <ann.panov@gmail.com>
2017-08-12 08:48:21 -0700

    not logging, screen only

--- config/_index_listings.bat
+++ config/_index_listings.bat
@@ -1,6 +1,6 @@
 cd "C:\Program Files\CompBoard\sandbox\"
 set TIMESTAMP=%DATE:~-4%%DATE:~-7,-5%%DATE:~-10,-8%_%time:~-11,2%%time:~-8,2%%time:~-5,2%
 set LOGFILE=availability_scraper_%TIMESTAMP%.log
-echo started at %TIMESTAMP% > %LOGFILE%
+rem echo started at %TIMESTAMP% > %LOGFILE%
 
-python ..\app\availability_scraper.py --product_config ..\config\out_of_stock.xlsx > %LOGFILE% 2>&1
+python ..\app\availability_scraper.py --product_config ..\config\out_of_stock.xlsx
