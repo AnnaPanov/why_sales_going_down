dba09d538293ff4cadfec907048858f85d650f32
Anna Panov <hannasl@hotmail.com>
2017-09-25 22:54:38 -0400

    timeout on requests

--- lib/problem_finders.py
+++ lib/problem_finders.py
@@ -364,7 +364,7 @@ def bloomingdales_problem_finder(url, config):
     #    'Accept': 'application/json',\
     #    'X-Macys-Webservice-Client-Id': 'ubmqtbg8k3kmwuszkcv2ng5z'\
     }
-    page = requests.get(url, headers=headers)
+    page = requests.get(url, headers=headers, timeout=10)
     if ("hoose your items" in page.text):
         return ProductProblem(CONFIG_ERROR, "this link is for a collection, but not for specific product")
     for block in page.text.split("</script"):
@@ -544,7 +544,7 @@ def jcpenney_problem_finder(url, config):
             return ProductProblem(CONFIG_ERROR, "url does not have a '/blahblah?' in it, so I cannot determine the SKU")
     sku = sku.groups()[0]
     inventory_url = "http://www.jcpenney.com/v1/product-aggregator/%s/inventory" % sku
-    inventory_response = requests.get(inventory_url)
+    inventory_response = requests.get(inventory_url, timeout=10)
     if (inventory_response.text.strip() == ""):
         return ProductProblem(STOCKOUT, "item cannot be added to bag")
     try:
@@ -747,7 +747,7 @@ def saks_problem_finder(url, config):
     headers = {\
         'user-agent': 'Availability Checker/0.0.1',\
     }
-    page = requests.get(url, headers=headers)
+    page = requests.get(url, headers=headers, timeout=10)
     if page.status_code != 200:
         raise ProductProblemException(ProductProblem(PAGE_NOT_LOADED, "response code %d" % page.status_code))
     if not isinstance(page.text, str):
@@ -792,7 +792,7 @@ def _load_product_page(url, config):
     try:
         logging.info("loading from '%s' ..." % str(url))
         if (url[0:7] == "file://"): return LoadFile(url[7:]) # mainly for tests, but who knows
-        response = requests.get(url)
+        response = requests.get(url, timeout=10)
         logging.info("^ status_code: %d, content_length: %d" % (response.status_code, len(response.text)))
     except:
         raise ProductProblemException(ProductProblem(PAGE_NOT_LOADED, "%s" % str(sys.exc_info())))
